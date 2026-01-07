adbe218655e25ad6c2cc88e9da029bd227d73f77
keshavbansal5766 <keshav22.dlp@gmail.com>
2026-01-08 01:10:56 +0530

    created Login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Feed from "./pages/Feed";
+import Messages from "./pages/Messages";
+import Connections from "./pages/Connections";
+import ChatBox from "./pages/ChatBox";
+import Discover from "./pages/Discover";
+import Profile from "./pages/Profile";
+import CreatePost from "./pages/CreatePost";
 
 const App = () => {
-  return <div className="text-red-500">App</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Login />}>
+          <Route index element={<Feed />} />
+          <Route path="messages" element={<Messages />} />
+          <Route path="messages/:userId" element={<ChatBox />} />
+          <Route path="connections" element={<Connections />} />
+          <Route path="discover" element={<Discover />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="profile/:profileId" element={<Profile />} />
+          <Route path="create-post" element={<CreatePost />} />
+        </Route>
+      </Routes>
+    </>
+  );
 };
 
 export default App;
