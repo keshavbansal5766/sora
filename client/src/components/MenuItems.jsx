2cce5f29c2966f3de96514c4533fcd686ce1dd99
keshavbansal5766 <keshav22.dlp@gmail.com>
2026-01-09 01:19:09 +0530

    Created Side bar with menu items components and enable multi account handling

---
+++ client/src/components/MenuItems.jsx
@@ -0,0 +1,27 @@
+import { menuItemsData } from "../assets/assets";
+import { NavLink } from "react-router-dom";
+
+const MenuItems = ({ setSidebarOpen }) => {
+  return (
+    <div className="px-6 text-gray-600 space-y-1 font-medium">
+      {menuItemsData.map(({ to, label, Icon }) => (
+        <NavLink
+          key={label}
+          to={to}
+          end={to === "/"}
+          onClick={() => setSidebarOpen(false)}
+          className={({ isActive }) =>
+            `flex items-center gap-3 px-3.5 py-2 rounded-xl ${
+              isActive ? "bg-indigo-50 text-indigo-700" : "hover:bg-gray-50"
+            }`
+          }
+        >
+          <Icon className="w-5 h-5" />
+          {label}
+        </NavLink>
+      ))}
+    </div>
+  );
+};
+
+export default MenuItems;
