14f8efa6842c2d7e01d94b746e2441a7f39cdfbd
keshavbansal5766 <keshav22.dlp@gmail.com>
2026-01-12 16:45:51 +0530

    created profile component

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { dummyPostsData, dummyUserData } from "../assets/assets";
 import Loading from "../components/Loading";
 import UserProfileInfo from "../components/UserProfileInfo";
+import PostCard from "../components/PostCard";
+import moment from "moment";
 
 const Profile = () => {
   const { profileId } = useParams();
   const [user, setUser] = useState(null);
   const [posts, setPosts] = useState([]);
   const [activeTab, setActiveTab] = useState("posts");
-  const [showEdit, setShowEdit] = useState("false");
+  const [showEdit, setShowEdit] = useState(false);
 
   const fetchUser = async () => {
     setUser(dummyUserData);
@@ -42,7 +44,66 @@ const Profile = () => {
             setShowEdit={setShowEdit}
           />
         </div>
+
+        {/* Tabs */}
+        <div className="mt-6">
+          <div className="bg-white rounded-xl shadow p-1 flex max-w-md mx-auto">
+            {["posts", "media", "likes"].map((tab) => (
+              <button
+                key={tab}
+                onClick={() => setActiveTab(tab)}
+                className={`flex-1 px-4 py-2 text-sm font-medium rounded-lg transition-colors cursor-pointer ${
+                  activeTab === tab
+                    ? "bg-indigo-600 text-white"
+                    : "text-gray-600 hover:text-gray-900"
+                } `}
+              >
+                {tab.charAt(0).toUpperCase() + tab.slice(1)}
+              </button>
+            ))}
+          </div>
+
+          {/* posts */}
+          {activeTab === "posts" && (
+            <div className="mt-6 flex flex-col items-center gap-6">
+              {posts.map((post) => (
+                <PostCard key={post._id} post={post} />
+              ))}
+            </div>
+          )}
+
+          {/* media */}
+          {activeTab === "media" && (
+            <div className="flex flex-wrap mt-6 max-w-6xl">
+              {posts
+                .filter((post) => post.image_urls.length > 0)
+                .map((post) => (
+                  <>
+                    {post.image_urls.map((image, index) => (
+                      <Link
+                        key={index}
+                        target="_blank"
+                        to={image}
+                        className="relative group"
+                      >
+                        <img
+                          src={image}
+                          alt=""
+                          className="-64 aspect-video object-cover"
+                        />
+                        <p className="absolute bottom-0  right-0 text-xs p-1 px-3 backdrop-blur-xl text-whiten opacity-0 group-hover:opacity-100 transition duration-300">
+                          Posted {moment(post.createdAt).fromNow()}
+                        </p>
+                      </Link>
+                    ))}
+                  </>
+                ))}
+            </div>
+          )}
+        </div>
       </div>
+      {/* Edit Profile Modal */}
+      {showEdit && <p>Show Edit profile</p>}
     </div>
   ) : (
     <Loading />
