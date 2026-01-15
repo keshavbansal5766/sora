5329bbcf7b8c614afe316f1f7ef0959c99ebbc0a
keshavbansal5766 <keshav22.dlp@gmail.com>
2026-01-16 00:24:39 +0530

    Creating Connection model

---
+++ server/models/Connection.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose"
+
+const connectionSchema = new mongoose.Schema({
+    from_user_id: {type: String, ref: "User", required: true},
+    to_user_id: {type: String, ref: "User", required: true},
+      status: {type: String, enum: ["pending", "accepted"], default: "pending"}
+}, {timeStamps: true})
+
+const connection = mongoose.models.Connection || mongoose.model("Connection", connectionSchema)
+
+export default Connection
