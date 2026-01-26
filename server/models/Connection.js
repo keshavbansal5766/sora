1715d0043c5b8f578c5dcab760559175fbd8b748
keshavbansal5766 <keshav22.dlp@gmail.com>
2026-01-27 01:04:44 +0530

    fixing connectionSchema bug

--- server/models/Connection.js
+++ server/models/Connection.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose"
 const connectionSchema = new mongoose.Schema({
     from_user_id: {type: String, ref: "User", required: true},
     to_user_id: {type: String, ref: "User", required: true},
-      status: {type: String, enum: ["pending", "accepted"], default: "pending"}
-}, {timeStamps: true})
+    status: {type: String, enum: ["pending", "accepted"], default: "pending"}
+}, {timestamps: true})
 
 const Connection = mongoose.models.Connection || mongoose.model("Connection", connectionSchema)
 
