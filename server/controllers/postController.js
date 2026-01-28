aad573b0871cf5f8a0ffaee7188abfc9d066a691
keshavbansal5766 <keshav22.dlp@gmail.com>
2026-01-28 22:44:10 +0530

    create addPost, getFeedPosts, likePost, and create routes

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,5 +1,7 @@
 import imageKit from "../configs/imageKit.js"
 import Post from "../models/Post.js"
+import fs from "fs"
+import User from "../models/User.js"
 
 export const addPost = async (req, res) => {
     try {
@@ -9,7 +11,7 @@ export const addPost = async (req, res) => {
         let image_urls = []
 
         if(images.length) {
-            image_urls = Promise.all(
+            image_urls = await Promise.all(
                 images.map(async(image) => {
                     const response = await imageKit.files.upload({
                     file: fs.createReadStream(image.path),
@@ -39,3 +41,51 @@ export const addPost = async (req, res) => {
         res.json({success: false, message: error.message})
     }
 }
+
+// Get Posts
+export const getFeedPosts = async (req, res) => {
+    try {
+        const {userId} = req.auth()
+        const user = await User.findById(userId)
+
+        // User connections and followings
+        const userIds = [userId, ...(user.connections || []), ...(user.following || [])]
+        const posts = await Post.find({user: {$in: userIds}}).populate("user").sort({createdAt: -1}).limit(20)
+        res.json({success: true, posts})
+    } catch (error) {
+        console.log(error.message)
+        res.json({success: false, message: error.message})
+    }
+}
+
+
+// Like Post
+export const likePost = async (req, res) => {
+    try {
+        const {userId} = req.auth()
+        const {postId} = req.body
+
+        const post = await Post.findById(postId).select("likes_count")
+        if (!post) {
+        return res.json({ success: false, message: "Post not found" })
+        }
+
+        const isLiked = post.likes_count.includes(userId)
+
+        await Post.findByIdAndUpdate(
+        postId,
+        isLiked
+            ? { $pull: { likes_count: userId } }
+            : { $addToSet: { likes_count: userId } }
+        )
+
+        res.json({
+        success: true,
+        message: isLiked ? "Post unliked" : "Post liked"
+        })
+    } catch (error) {
+        console.log(error.message)
+        res.json({success: false, message: error.message})
+    }
+}
+
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import { inngest, functions } from "./inngest/index.js";
 import { serve } from "inngest/express";
 import { clerkMiddleware } from "@clerk/express";
 import userRouter from "./routes/userRoutes.js";
+import postRouter from "./routes/postRoutes.js";
 
 const app = express();
 
@@ -19,6 +20,7 @@ app.get("/", (req, res) => res.send("server is running"));
 // Set up the "/api/inngest" (recommended) routes with the serve handler
 app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/user", userRouter);
+app.use("/api/post", postRouter);
 
 const PORT = process.env.PORT || 4000;
 
