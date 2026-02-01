e756d14f4bf8955e8caeba80f7c7495c59496cf0
keshavbansal5766 <keshav22.dlp@gmail.com>
2026-02-01 20:42:20 +0530

    create getChatMessages controller

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -68,9 +68,37 @@ export const sendMessage = async (req, res) => {
       "from_user_id",
     );
 
-    if(connections[to_user_id]) {
-        connections[to_user_id].write(`data: ${JSON.stringify(messageWithUserData)}\n\n`)
+    if (connections[to_user_id]) {
+      connections[to_user_id].write(
+        `data: ${JSON.stringify(messageWithUserData)}\n\n`,
+      );
     }
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// Get Chat Messages
+export const getChatMessages = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { to_user_id } = req.body;
+
+    const messages = await Message.find({
+      $or: [
+        { from_user_id: userId, to_user_id },
+        { from_user_id: to_user_id, to_user_id: userId },
+      ],
+    }).sort({ created_at: -1 });
+
+    // mark messages as seen
+    await Message.updateMany(
+      { from_user_id: to_user_id, to_user_id: userId },
+      { seen: true },
+    );
+
+    res.json({ success: true, messages });
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
