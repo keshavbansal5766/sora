1d0ab9efaa20cac75fb6711163fa52249ec2aa0f
keshavbansal5766 <keshav22.dlp@gmail.com>
2026-02-05 02:39:19 +0530

    implment fetching updated post in feed and like and dislike update in backend feature

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -4,6 +4,9 @@ import React, { useState } from "react";
 import { dummyUserData } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useAuth } from "@clerk/clerk-react";
+import api from "../api/axios";
+import toast from "react-hot-toast";
 
 const PostCard = ({ post }) => {
   const [likes, setLikes] = useState(post.likes_count);
@@ -12,10 +15,37 @@ const PostCard = ({ post }) => {
 
   const postWithHashtags = post.content.replace(
     /(#\w+)/g,
-    '<span class="text-indigo-600">$1</span>'
+    '<span class="text-indigo-600">$1</span>',
   );
 
-  const handleLike = async () => {};
+  const { getToken } = useAuth();
+
+  const handleLike = async () => {
+    try {
+      const { data } = await api.post(
+        `/api/post/like`,
+        { postId: post._id },
+        {
+          headers: { Authorization: `Bearer ${await getToken()}` },
+        },
+      );
+
+      if (data.success) {
+        toast.success(data.message);
+        setLikes((prev) => {
+          if (prev.includes(currentUser._id)) {
+            return prev.filter((id) => id !== currentUser._id);
+          } else {
+            return [...prev, currentUser._id];
+          }
+        });
+      } else {
+        toast(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
 
   return (
     <div className="bg-white rounded-xl shadow p-4 space-y-4 w-full max-w-2xl">
